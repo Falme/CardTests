b4441304f4d710ed81d694c600e7afce65a831cd
Falme Streamless <falme_streamless@hotmail.com>
2025-04-07 22:06:13 -0300

    Added suits to the card random generation

--- scripts/card.gd
+++ scripts/card.gd
@@ -1,4 +1,4 @@
-class_name Rank
+class_name Card
 extends Label
 
 var _timed : float = 0
@@ -13,11 +13,21 @@ var _ranks = {
 var _min_rank = 1
 var _max_rank = 13
 
+var _suits = {
+	1:"Diamonds",
+	2:"Clubs",
+	3:"Hearts",
+	4:"Spades"
+}
+
+var _min_suit = 1
+var _max_suit = 4
+
 func _process(delta: float) -> void:
 	_timed += delta
 
 	if _timed >= 1:
-		text = number_to_rank(get_rankable_number())
+		text =  number_to_suit(get_suitable_number())+ " : " +number_to_rank(get_rankable_number())
 		_timed -= 1
 
 func get_rankable_number() -> int:
@@ -30,3 +40,10 @@ func number_to_rank(number : int) -> String:
 		return _ranks[number]
 	
 	return str(number)
+
+func get_suitable_number() -> int:
+	return randi_range(_min_suit,_max_suit)
+
+func number_to_suit(number: int) -> String:
+	number = clamp(number, _min_suit, _max_suit)
+	return _suits[number]
